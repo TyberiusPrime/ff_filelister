0b0e6814d86f53b2e5496c85cf40ae37cfdde503
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2021-10-27 09:01:23 +0200

    Working version

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    #mozillapkgs = {
+    #url = "github:mozilla/nixpkgs-mozilla";
+    #flake = false;
+    #};
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        #pkgs = nixpkgs.legacyPackages."${system}";
+
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.stable."1.56.0".default.override {
+#          targets = [ "x86_64-unknown-linux-musl" ];
+        };
+
+        # Override the version used in naersk
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust;
+          rustc = rust;
+        };
+      in rec {
+        # `nix build`
+        packages.my-project = naersk-lib.buildPackage {
+          pname = "ff_filelister";
+          root = ./.;
+        };
+        defaultPackage = packages.my-project;
+
+        # `nix run`
+        apps.my-project = utils.lib.mkApp { drv = packages.my-project; };
+        defaultApp = apps.my-project;
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          # supply the specific rust version
+          nativeBuildInputs = [ rust  pkgs.rust-analyzer pkgs.git pkgs.cargo-udeps];
+        };
+      });
+}
+# {
---
+++ src/main.rs
@@ -0,0 +1,117 @@
+use anyhow::{bail, Context, Result};
+use sha1::{Digest, Sha1};
+use std::path::{Path, PathBuf};
+use std::process;
+use std::str::FromStr;
+use std::time::SystemTime;
+
+fn list_dir(path: impl AsRef<Path>) -> Result<String> {
+    let rg_args = &[
+        "--type",
+        "js",
+        "--type",
+        "lua",
+        "--type",
+        "vim",
+        "--type-add",
+        "tera:*.tera",
+        "--type-add",
+        "cfg:*.cfg",
+        "--type",
+        "cfg",
+        "--type-add",
+        "toml:*.toml",
+        "--type",
+        "toml",
+        "--type",
+        "rust",
+        "--type",
+        "py",
+        "--type",
+        "cython",
+        "--type",
+        "rst",
+        "--type",
+        "md",
+        "--type",
+        "html",
+        "--type",
+        "txt",
+        "--type",
+        "nix",
+        "-g!code/venv",
+        "-g!results",
+        "-g!cache",
+        "-g!incoming",
+        "-g!manual_code",
+        "-g!*.egg-info",
+        "-j",
+        "40",
+        "--files",
+    ];
+    let p = process::Command::new("rg")
+        .args(rg_args)
+        .current_dir(path)
+        .output()?;
+    let res = std::str::from_utf8(&p.stdout)?.to_string();
+
+    let lines = res.split("\n");
+    let (no_test, test): (Vec<&str>, Vec<&str>) = lines.partition(|x| !x.contains("test"));
+    let res: String = no_test.join("\n") + &test.join("\n");
+
+    Ok(res)
+}
+
+fn main() {
+    let r = inner_main();
+    match r {
+        Ok(_) => {}
+        Err(x) => println!("An error occured {:?}", x),
+    };
+}
+fn inner_main() -> Result<()> {
+    let cache_dir = dirs::home_dir()
+        .expect("No home")
+        .join(".cache/ff_filelister");
+    std::fs::create_dir_all(&cache_dir)
+        .with_context(|| format!("Could not create cache dir {:?}", &cache_dir))?;
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        panic!("Wrong number of arguments")
+    }
+    let target = &args[1];
+    if !PathBuf::from_str(&target)?.exists() {
+        bail!("Could not find target directory {}", target);
+    }
+
+    let mut hasher = Sha1::new();
+    // process input message
+    hasher.update(args[1].as_bytes());
+    let result = format!("{:x}", hasher.finalize());
+
+    let cache_file = cache_dir.join(result);
+    let rebuild = if !cache_file.exists() {
+        let raw = list_dir(&args[1])?;
+        std::fs::write(&cache_file, raw)?;
+        false
+    } else {
+        true
+    };
+    print!("{}\n", std::fs::read_to_string(&cache_file)?);
+
+    //make telescope allow
+    unsafe {
+        libc::close(1);
+    }
+    if rebuild {
+        if cache_file.metadata()?.modified()?
+            < SystemTime::now() - std::time::Duration::from_secs(60 * 5)
+        {
+            let raw = list_dir(&args[1])?;
+            std::fs::write(&cache_file, raw)?;
+        }
+    }
+
+    Ok(())
+}
