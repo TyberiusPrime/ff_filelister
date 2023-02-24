8e69787c162af9933d187d3c5a8d7b73ffead251
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2023-02-24 10:04:48 +0100

    take rg args from cmd

--- flake.nix
+++ flake.nix
@@ -5,11 +5,9 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     naersk.inputs.nixpkgs.follows = "nixpkgs";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    rust-overlay.inputs.flake-utils.follows = "nixpkgs";
+    #rust-overlay.inputs.flake-utils.follows = "nixpkgs";
     nixpkgs = {
-      url =
-        "github:NixOS/nixpkgs?rev=7e9b0dff974c89e070da1ad85713ff3c20b0ca97"; # that's 21.05
-
+      url = "github:NixOS/nixpkgs?rev=7e9b0dff974c89e070da1ad85713ff3c20b0ca97"; # that's 21.05
     };
 
     #mozillapkgs = {
@@ -18,48 +16,52 @@
     #};
   };
 
-  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        #pkgs = nixpkgs.legacyPackages."${system}";
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+    rust-overlay,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      #pkgs = nixpkgs.legacyPackages."${system}";
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rust = pkgs.rust-bin.stable."1.56.0".default.override {
+        extensions = ["rustfmt" "clippy"];
 
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rust = pkgs.rust-bin.stable."1.56.0".default.override {
-          extensions = [ "rustfmt" "clippy" ];
+        #          targets = [ "x86_64-unknown-linux-musl" ];
+      };
 
-          #          targets = [ "x86_64-unknown-linux-musl" ];
+      # Override the version used in naersk
+      naersk-lib = naersk.lib."${system}".override {
+        cargo = rust;
+        rustc = rust;
+      };
+    in rec {
+      # `nix build`
+      packages.my-project = naersk-lib.buildPackage {
+        pname = "ff_filelister";
+        root = ./.;
+        buildInput = [pkgs.ripgrep pkgs.breakpointHook];
+        overrideMain = old: {
+          postUnpack = ''
+            substituteInPlace /build/source/src/main.rs --replace "/usr/bin/rg" "${pkgs.ripgrep}/bin/rg"
+          '';
         };
+      };
+      defaultPackage = packages.my-project;
 
-        # Override the version used in naersk
-        naersk-lib = naersk.lib."${system}".override {
-          cargo = rust;
-          rustc = rust;
-        };
-      in rec {
-        # `nix build`
-        packages.my-project = naersk-lib.buildPackage {
-          pname = "ff_filelister";
-          root = ./.;
-          buildInput = [ pkgs.ripgrep pkgs.breakpointHook ];
-          overrideMain = old: {
-            postUnpack = ''
-              substituteInPlace /build/source/src/main.rs --replace "/usr/bin/rg" "${pkgs.ripgrep}/bin/rg"
-            '';
-          };
-        };
-        defaultPackage = packages.my-project;
+      # `nix run`
+      apps.my-project = utils.lib.mkApp {drv = packages.my-project;};
+      defaultApp = apps.my-project;
 
-        # `nix run`
-        apps.my-project = utils.lib.mkApp { drv = packages.my-project; };
-        defaultApp = apps.my-project;
-
-        # `nix develop`
-        devShell = pkgs.mkShell {
-          # supply the specific rust version
-          nativeBuildInputs =
-            [ rust pkgs.rust-analyzer pkgs.git pkgs.cargo-udeps ];
-        };
-      });
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        # supply the specific rust version
+        nativeBuildInputs = [rust pkgs.rust-analyzer pkgs.git pkgs.cargo-udeps];
+      };
+    });
 }
 # {
+
--- src/main.rs
+++ src/main.rs
@@ -5,57 +5,67 @@ use std::process;
 use std::str::FromStr;
 use std::time::SystemTime;
 
-fn list_dir(path: impl AsRef<Path>) -> Result<String> {
-    let rg_args = &[
-        "--type",
-        "js",
-        "--type",
-        "lua",
-        "--type",
-        "vim",
-        "--type-add",
-        "tera:*.tera",
-        "--type-add",
-        "cfg:*.cfg",
-        "--type",
-        "cfg",
-        "--type-add",
-        "toml:*.toml",
-        "--type",
-        "toml",
-        "--type",
-        "rust",
-        "--type",
-        "py",
-        "--type",
-        "cython",
-        "--type",
-        "rst",
-        "--type",
-        "md",
-        "--type",
-        "html",
-        "--type",
-        "txt",
-        "--type",
-        "nix",
-        "--type",
-        "ts",
-        "-g!code/venv",
-        "-g!results",
-        "-g!cache",
-        "-g!incoming",
-        "-g!manual_code",
-        "-g!*.egg-info",
-        "-j",
-        "40",
-        "--files",
-    ];
+fn list_dir(path: impl AsRef<Path>, rg_args: Option<Vec<String>>) -> Result<String> {
+    let rg_args = match rg_args {
+        Some(x) => x,
+        None => vec![
+            "--type",
+            "js",
+            "--type",
+            "lua",
+            "--type",
+            "vim",
+            "--type-add",
+            "tera:*.tera",
+            "--type-add",
+            "cfg:*.cfg",
+            "--type",
+            "cfg",
+            "--type-add",
+            "toml:*.toml",
+            "--type",
+            "toml",
+            "--type",
+            "rust",
+            "--type",
+            "py",
+            "--type",
+            "cython",
+            "--type",
+            "rst",
+            "--type",
+            "md",
+            "--type",
+            "html",
+            "--type",
+            "txt",
+            "--type",
+            "nix",
+            "--type",
+            "ts",
+            "--type-add",
+            "svelte:*.svelte",
+            "--type",
+            "svelte",
+            "-g!code/venv",
+            "-g!results",
+            "-g!cache",
+            "-g!incoming",
+            "-g!manual_code",
+            "-g!*.egg-info",
+            "-j",
+            "40",
+            "--files",
+        ].iter().map(|x| x.to_string()).collect()
+    };
     let p = process::Command::new("/usr/bin/rg")
         .args(rg_args)
         .current_dir(path)
-        .output().context("Failed to run /usr/bin/rg")?;
-    let res = std::str::from_utf8(&p.stdout).context("rg output was not utf8")?.to_string();
+        .output()
+        .context("Failed to run /usr/bin/rg")?;
+    let res = std::str::from_utf8(&p.stdout)
+        .context("rg output was not utf8")?
+        .to_string();
 
     let lines = res.split("\n");
     let (no_test, test): (Vec<&str>, Vec<&str>) = lines.partition(|x| !x.contains("test"));
@@ -79,15 +89,29 @@ fn inner_main() -> Result<()> {
         .with_context(|| format!("Could not create cache dir {:?}", &cache_dir))?;
 
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 3 {
-        println!("Wrong number of arguments. Needs to be [timeout_in_seconds] [folder]");
+    if args.len() < 3 {
+        println!("Wrong number of arguments. Needs to be at least [timeout_in_seconds] [folder]");
+        println!("Optionally pass in '--' after, followed by rg arguments");
         std::process::exit(0);
     }
     let timeout: u32 = str::parse(&args[1]).context("timeout not a number")?;
     let target = &args[2];
-    if !PathBuf::from_str(&target).context("could not cerate path from argument")?.exists() {
+    if !PathBuf::from_str(&target)
+        .context("could not cerate path from argument")?
+        .exists()
+    {
         bail!("Could not find target directory {}", target);
     }
+    let rg_args = if args.len() > 3 {
+        if &args[3] != "--" {
+            let r: Vec<String> = (&args[4..]).iter().map(|x| x.to_string()).collect();
+            Some(r)
+        } else {
+            bail!("if passing in rg arguments, third argument must be \"--\"");
+        }
+    } else {
+        None
+    };
 
     let mut hasher = Sha1::new();
     // process input message
@@ -96,24 +120,31 @@ fn inner_main() -> Result<()> {
 
     let cache_file = cache_dir.join(result);
     let rebuild = if !cache_file.exists() {
-        let raw = list_dir(&target).context("failed to listdir")?;
+        let raw = list_dir(&target, rg_args.clone()).context("failed to listdir")?;
         std::fs::write(&cache_file, raw).context("failed to write cache_file")?;
         false
     } else {
         true
     };
-    println!("{}", std::fs::read_to_string(&cache_file).context("failed to read cache_file")?);
+    println!(
+        "{}",
+        std::fs::read_to_string(&cache_file).context("failed to read cache_file")?
+    );
 
     //make telescope allow
     unsafe {
         libc::close(1);
     }
     if rebuild {
-        if cache_file.metadata().context("no cache file metadata")?.modified().context("no cachefile modified time")?
+        if cache_file
+            .metadata()
+            .context("no cache file metadata")?
+            .modified()
+            .context("no cachefile modified time")?
             < SystemTime::now() - std::time::Duration::from_secs(timeout.into())
         {
             //update it
-            let raw = list_dir(&target).context("failed to listdir")?;
+            let raw = list_dir(&target, rg_args).context("failed to listdir")?;
             std::fs::write(&cache_file, raw)?;
         }
     }
